66fc1edaf2c51832ecbccf415886df8d4e48efca
RaitoRyu 5816 <longnguyen58162001@gmail.com>
2025-03-19 23:26:05 +0700

    Add deployments for zookeeper and kafka

--- test.py
+++ test.py
@@ -1,15 +1,21 @@
-import boto3
-from botocore.client import Config
 import requests
 
+def get_weather_data(lon, lat, api_key):
+    url = f"https://api.openweathermap.org/data/3.0/onecall/overview?lon={lon}&lat={lat}&appid={api_key}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Failed to fetch data: {response.status_code}")
+        return None
 
-url = "https://s3.amazonaws.com/tripdata/JC-202502-citibike-tripdata.csv.zip"
-local_path = "data/citibike/JC-202502-citibike-tripdata.csv.zip"
-
-response = requests.get(url, stream=True)
-print(response.status_code)
-if response.status_code == 200:
-    with open(local_path, 'wb') as f:
-        for chunk in response.iter_content(1024):
-            f.write(chunk)
-    print(f"Downloaded {local_path}")
+if __name__ == "__main__":
+    lon = -11.8092
+    lat = 51.509865
+    api_key = "YOUR_API_KEY_HERE"
+    
+    weather_data = get_weather_data(lon, lat, api_key)
+    
+    if weather_data:
+        print(weather_data)
